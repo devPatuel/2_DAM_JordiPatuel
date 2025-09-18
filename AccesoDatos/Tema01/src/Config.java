228516ba6e044ea2c17520471b76b903606a86e2
devPatuel <devpatuel@outlook.com>
2025-09-18 19:31:48 +0200

    actv

--- AccesoDatos/Tema01/src/Config.java
+++ AccesoDatos/Tema01/src/Config.java
@@ -1,3 +1,3 @@
 public class Config {
-    public static final String RUTA = "C:\\Users\\pocap\\OneDrive\\Escritorio\\2_DAM\\2_DAM_JordiPatuel\\AccesoDatos";
+    public static final String RUTA = "C:\\Users\\pocap\\OneDrive\\Escritorio\\2_DAM\\2_DAM_JordiPatuel\\AccesoDatos\\Tema01";
 }
